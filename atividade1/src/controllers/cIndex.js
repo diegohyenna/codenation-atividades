8e0cbf30da76c1d9f4f88b3e57473af366c6b6ea
diegohyenna <dgmartins87@hotmail.com>
2019-05-22 13:41:23 -0300

    atualizando o master clean

--- atividade1/src/controllers/cIndex.js
+++ atividade1/src/controllers/cIndex.js
@@ -30,17 +30,3 @@ exports.get = ((req, res) => {
       res.send(error)
     })
 })
-
-exports.post = (req, res) => {
-  res.status(201).send('Requisição recebida com sucesso!');
-}
-
-exports.put = (req, res) => {
-  let id = req.params.id;
-  res.status(201).send(`Requisição recebida com sucesso! ${id}`);
-}
-
-exports.delete = (req, res) => {
-  let id = req.params.id;
-  res.status(200).send(`Requisição recebida com sucesso! ${id}`);
-}
